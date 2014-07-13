072bb7bcff2c518936a3132b17307964653fb6a1
Pavani Allam <allampavani@gmail.com>
2014-07-12 23:26:04 -0400

    Assignment Week1 Exploratory data Analysis

--- plot3.R
+++ plot3.R
@@ -1,4 +1,5 @@
 consumption <- read.csv("./household_power_consumption.txt",sep=";",na.strings='?',header=TRUE,colClasses = c("character",rep("factor",1),rep("numeric",7)))
+
 selection<-consumption[consumption$Date %in% c("1/2/2007","2/2/2007"),]
 
 plot(strptime(paste(selection$Date,selection$Time),"%d/%m/%Y %H:%M:%S"),
