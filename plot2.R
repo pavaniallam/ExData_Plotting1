278325a5a63ba1feae59885be9360607af014452
Pavani Allam <allampavani@gmail.com>
2014-07-12 23:15:25 -0400

    Plot2.R

--- plot2.R
+++ plot2.R
@@ -4,4 +4,5 @@ selection<-consumption[consumption$Date %in% c("1/2/2007","2/2/2007"),]
 plot(strptime(paste(selection$Date,selection$Time),"%d/%m/%Y %H:%M:%S"),selection$Global_active_power,ylab="Global Active Power (kilowatts)",xlab="",type="l")
 
 dev.copy(png, file = "plot2.png")  ## copy the plot to a PNG file
+
 dev.off()  ## close PNG device
